854d864e3f24ce6fe1ced5e6773f4103975b03d4
Usman Mahmood <usmanwardag@gmail.com>
2016-02-09 08:34:56 +0500

    Adds UDP client and server code

---
+++ UDP/UDPServer.py
@@ -0,0 +1,14 @@
+from socket import *
+
+serverPort = 1200
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('', serverPort))
+
+print 'The server is ready to receive'
+
+while 1:
+    message, clientAddress = serverSocket.recvfrom(2048)
+    modifiedMessage = message.upper()
+    serverSocket.sendto(modifiedMessage, clientAddress)
+
+serverSocket.bind(('', serverPort))
